6da53833960e4633864664dfa468c95fa4f2407b
ITh0rn <angelo.dalfonso@student.univaq.it>
2019-11-24 17:25:20 +0100

    Create Poll init [Servlet/template]

--- src/poolweb/controller/CreatePoll.java
+++ src/poolweb/controller/CreatePoll.java
@@ -2,54 +2,45 @@ package poolweb.controller;
 
 import poolweb.data.dao.PoolWebDataLayer;
 import poolweb.data.model.Poll;
-import poolweb.data.model.User;
 import poolweb.framework.data.DataException;
 import poolweb.framework.result.FailureResult;
 import poolweb.framework.result.SplitSlashesFmkExt;
 import poolweb.framework.result.TemplateManagerException;
 import poolweb.framework.result.TemplateResult;
+
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet(name = "GetUser")
-public class GetUser extends PoolWebBaseController {
-
+public class CreatePoll extends PoolWebBaseController  {
     @Override
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException {
-        /* Possibile Implementare Controlli sui dati in input nella request, come validate, sanitaze o controlli su input mancanti*/
         try {
-            action_user(request, response);
-        } catch (IOException ioexc) {
-            ioexc.printStackTrace();
-        }
-    }
-
-    private void action_user(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        try {
-            List<Poll> poll = ((PoolWebDataLayer) request.getAttribute("datalayer")).getPollDAO().getAllPoll();
-            if (poll != null) {
-                request.setAttribute("poll", poll);
-                request.setAttribute("page_title", "Home"); //Titolo da iniettare nel template con freeMarker
-                TemplateResult res = new TemplateResult(getServletContext());
-                request.setAttribute("strip_slashes", new SplitSlashesFmkExt());
-                res.activate("home.ftl", request, response);
-            } else {
-                request.setAttribute("message", "Unable to load Users");
-                action_error(request, response);
-            }
-        } catch (DataException ex) {
-            request.setAttribute("message", "Data access exception: " + ex.getMessage());
-            action_error(request, response);
-        } catch (TemplateManagerException e) {
+            action_poll(request, response);
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    //Necessario per gestire le return di errori
+    private void action_poll(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        try {
+            if (true) {
+                request.setAttribute("page_title", "Crea Sondaggio"); //Titolo da iniettare nel template con freeMarker
+                TemplateResult res = new TemplateResult(getServletContext());
+                request.setAttribute("strip_slashes", new SplitSlashesFmkExt());
+                res.activate("new_poll.ftl", request, response);
+            } else {
+                request.setAttribute("message", "Unable to load Users");
+                action_error(request, response);
+            }
+            } catch (TemplateManagerException e) {
+                e.printStackTrace();
+            }
+    }
+
+        //Necessario per gestire le return di errori
     private void action_error(HttpServletRequest request, HttpServletResponse response) {
         if (request.getAttribute("exception") != null) {
             (new FailureResult(getServletContext())).activate((Exception) request.getAttribute("exception"), request, response);
@@ -57,5 +48,4 @@ public class GetUser extends PoolWebBaseController {
             (new FailureResult(getServletContext())).activate((String) request.getAttribute("message"), request, response);
         }
     }
-
 }
