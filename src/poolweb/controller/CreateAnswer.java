a90a6a58cb998125da7c766a6f3859e911bc8cb0
im-slow <andrea.amicosante@gmail.com>
2019-11-29 16:14:45 +0100

    update

---
+++ src/poolweb/controller/CreateAnswer.java
@@ -0,0 +1,47 @@
+package poolweb.controller;
+
+import poolweb.framework.result.FailureResult;
+import poolweb.framework.result.SplitSlashesFmkExt;
+import poolweb.framework.result.TemplateManagerException;
+import poolweb.framework.result.TemplateResult;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CreateAnswer extends PoolWebBaseController {
+
+    @Override
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+        try {
+            action_answer(request, response);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void action_answer(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        try {
+            if (true) {
+                request.setAttribute("page_title", "Rispondi al Sondaggio"); //Titolo da iniettare nel template con freeMarker
+                TemplateResult res = new TemplateResult(getServletContext());
+                request.setAttribute("strip_slashes", new SplitSlashesFmkExt());
+                res.activate("new_answer.ftl", request, response);
+            } else {
+                request.setAttribute("message", "Unable to load Users");
+                action_error(request, response);
+            }
+        } catch (TemplateManagerException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Necessario per gestire le return di errori
+    private void action_error(HttpServletRequest request, HttpServletResponse response) {
+        if (request.getAttribute("exception") != null) {
+            (new FailureResult(getServletContext())).activate((Exception) request.getAttribute("exception"), request, response);
+        } else {
+            (new FailureResult(getServletContext())).activate((String) request.getAttribute("message"), request, response);
+        }
+    }
+}
---
+++ web/templates/new_answer.ftl
@@ -0,0 +1,14 @@
+<section id="gtco-contact-form" class="bg-white pt-5">
+    <div class="container">
+        <div class="section-content">
+            <!-- Section Title -->
+            <div class="title-wrap">
+                <h2 class="section-title">Titolo sondaggio</h2>
+                <p class="section-sub-title">Testo di apertura<br>Quando hai finito  di compilare il sondaggio premi il bottone per inviare la tua risposta</p>
+
+            </div>
+            <!-- End of Section Title -->
+
+        </div>
+    </div>
+</section>
