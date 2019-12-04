aaa153b3fabc91dc0a50e15d83cd7f4bb1e2a758
ITh0rn <angelo.dalfonso@student.univaq.it>
2019-12-04 11:40:04 +0100

    Update question insert and Template fix

--- src/poolweb/controller/InsertQuestions.java
+++ src/poolweb/controller/InsertQuestions.java
@@ -1,28 +1,20 @@
 package poolweb.controller;
 
 import poolweb.data.dao.PoolWebDataLayer;
-import poolweb.data.impl.PollImpl;
-import poolweb.data.impl.QuestionImpl;
-import poolweb.data.model.Poll;
 import poolweb.data.model.Question;
-import poolweb.data.model.User;
 import poolweb.framework.data.DataException;
 import poolweb.framework.result.FailureResult;
 import poolweb.framework.result.SplitSlashesFmkExt;
 import poolweb.framework.result.TemplateManagerException;
 import poolweb.framework.result.TemplateResult;
-import poolweb.util.ParserAnswer;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Enumeration;
-import java.util.List;
 import java.util.Map;
 import java.util.TreeSet;
 
-import static poolweb.util.ParserAnswer.parserAnswer;
 import static poolweb.util.ParserAnswer.randomQuestCode;
 
 public class InsertQuestions extends PoolWebBaseController{
@@ -41,42 +33,41 @@ public class InsertQuestions extends PoolWebBaseController{
     }
 
     private void action_poll(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        action_write(request, response);
-//        try {
-//            if (request.getParameterMap() != null) {
-//                TreeSet<String> indexes = new TreeSet<>();
-//                Map<String, String[]> parmap = request.getParameterMap();
-//                for(Map.Entry<String, String[]> entry : parmap.entrySet()) {
-//                    indexes.add((entry.getKey().substring(entry.getKey().length() - 1)));
-//                }
-//                for(String idx: indexes) {
-//                    // -- Here take the parameters for create a single Question Obj -- //
-//                    Question q;
-//                    q = ((PoolWebDataLayer) request.getAttribute("datalayer")).getQuestionDAO().createQuestion();
-//                    if (q != null) {
-//                        q.setQuestionText(request.getParameter("questname" +idx));
-//                        q.setNote(request.getParameter("info" +idx));
-//                        q.setMaximum(request.getParameter("max" +idx));
-//                        q.setMinimum(request.getParameter("min" +idx));
-//                        q.setQAnswer(request.getParameterValues("domanda"+idx));
-//                        q.setCode(randomQuestCode());
-////                        q.setPoll(); da gestire
-//                        q.setQuestionType(Question.QuestionType.valueOf("MULTIPLECHOISE")); //take from code
-//                        q.setMandatory(true); //take from code
-//                        q.setPositionNumber(3); //take from code
-//                        ((PoolWebDataLayer) request.getAttribute("datalayer")).getQuestionDAO().storeQuestion(q);
-//                        action_write(request, response);
-//                    }
-//                }
-//            } else {
-//                request.setAttribute("message", "Cannot update article: insufficient parameters");
-//                action_error(request, response);
-//
-//            }
-//        } catch (DataException ex) {
-//            request.setAttribute("message", "Cannot create Question");
-//            action_error(request, response);
-//        }
+        try {
+            if (request.getParameterMap() != null) {
+                TreeSet<String> indexes = new TreeSet<>();
+                Map<String, String[]> parmap = request.getParameterMap();
+                for(Map.Entry<String, String[]> entry : parmap.entrySet()) {
+                    indexes.add((entry.getKey().substring(entry.getKey().length() - 1)));
+                }
+                for(String idx: indexes) {
+                    // -- Here take the parameters for create a single Question Obj -- //
+                    Question q;
+                    q = ((PoolWebDataLayer) request.getAttribute("datalayer")).getQuestionDAO().createQuestion();
+                    if (q != null) {
+                        q.setQuestionText(request.getParameter("questname" +idx));
+                        q.setNote(request.getParameter("info" +idx));
+                        q.setMaximum(request.getParameter("max" +idx));
+                        q.setMinimum(request.getParameter("min" +idx));
+                        q.setQAnswer(request.getParameterValues("domanda"+idx));
+                        q.setCode(randomQuestCode());
+//                        q.setPoll(); da gestire
+                        q.setQuestionType(Question.QuestionType.valueOf("MULTIPLECHOISE")); //take from code
+                        q.setMandatory(true); //take from code
+                        q.setPositionNumber(3); //take from code
+                        ((PoolWebDataLayer) request.getAttribute("datalayer")).getQuestionDAO().storeQuestion(q);
+                        action_write(request, response);
+                    }
+                }
+            } else {
+                request.setAttribute("message", "Cannot update article: insufficient parameters");
+                action_error(request, response);
+
+            }
+        } catch (DataException ex) {
+            request.setAttribute("message", "Cannot create Question");
+            action_error(request, response);
+        }
     }
 
     private void action_write(HttpServletRequest request, HttpServletResponse response) {
